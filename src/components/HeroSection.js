a41e9a5a598605fae2f6d96cc21d6b6a5b13858a
Aditya Raj Chauhan <102533268+caringcoder@users.noreply.github.com>
2023-02-17 00:20:14 +0530

    languages section added

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -5,29 +5,31 @@ import Typewriter from 'typewriter-effect';
 function HeroSection() {
     return (
         <>
-            <div className="container h-25">
-                <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
-                    <div className="col-10 col-sm-8 col-lg-6">
-                        <img src={HeroImg} className="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy" />
-                    </div>
-                    <div className="col-lg-6">
-                        <p className="display-5 lh-1 mb-3 para">Hello,</p>
-                        <h1 ><Typewriter
-                            options={{
-                                strings: [`I'm Aditya Raj Chauhan`],
-                                autoStart: true,
-                                loop: true,
-                            }}
-                        /></h1>
-                        <h3 className='text-primary' >Full Stack Developer</h3>
-                        <h5>From India</h5>
-                        <div className="d-grid gap-2 d-md-flex justify-content-md-start">
-                            <button type="button" className="btn btn-primary btn-lg px-4 me-md-2">Hire Me</button>
+            <main>
+                <div className="container h-25">
+                    <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
+                        <div className="col-10 col-sm-8 col-lg-6">
+                            <img src={HeroImg} className="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy" />
+                        </div>
+                        <div className="col-lg-6">
+                            <p className="display-5 lh-1 mb-3 para">Hello,</p>
+                            <h1 ><Typewriter
+                                options={{
+                                    strings: [`I'm Aditya Raj Chauhan`],
+                                    autoStart: true,
+                                    loop: true,
+                                }}
+                            /></h1>
+                            <h3 className='text-primary' >Full Stack Developer</h3>
+                            <h5>From India</h5>
+                            <div className="d-grid gap-2 d-md-flex justify-content-md-start">
+                                <button type="button" className="btn btn-primary btn-lg px-4 me-md-2">Hire Me</button>
+                            </div>
                         </div>
                     </div>
+                    <hr />
                 </div>
-            <hr />
-            </div>
+            </main>
         </>
     )
 }
--- src/components/Languages.js
+++ src/components/Languages.js
@@ -1,9 +1,14 @@
 import React, { useEffect } from 'react'
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';
 import ReactJsLogo from '../assets/logo/ReactJs.png'
 import CSS3Logo from '../assets/logo/CSS3.png'
 import HTML5Logo from '../assets/logo/HTML5.png'
 import GithubLogo from '../assets/logo/Github.png'
 import MongoDbLogo from '../assets/logo/MongoDb.png'
+import NodeJsLogo from '../assets/logo/nodeJs.png'
+import CppLogo from '../assets/logo/cplus.png'
+import BootstrapLogo from '../assets/logo/bootstrap.png'
 
 
 const languagesSpam = [
@@ -32,33 +37,78 @@ const languagesSpam = [
         src: MongoDbLogo,
         alt: "MongoDb"
     },
+    {
+        name: "NodeJsLogo",
+        src: NodeJsLogo,
+        alt: "NodeJs"
+    },
+    {
+        name: "CplusLogo",
+        src: CppLogo,
+        alt: "Cpp"
+    },
+    {
+        name: "BootstrapLogo",
+        src: BootstrapLogo,
+        alt: "Bootstrap"
+    },
 ]
 
 
 
-
 function Languages() {
-     
-
     return (
         <>
-            <hr />
-            {/* <div className="container">
-                <div className="slider">
-                    <div className="slide-track">
-                        {
-                            languagesSpam.map((element, index) => {
-                                return (
-                                    <div className="slide" key={index}>
-                                        <img src={element.src} alt={element.alt} />
-                                    </div>
-                                )
-                            })
-                        }
-                    </div>
-                </div>
-            </div> */}
+            <div className="container ">
+                <hr />
+                <h2 className='text-center m-4'>Languages I'm Comfortable With</h2>
 
+                <div className="container">
+                    <CarouselProvider
+                        naturalSlideWidth={5}
+                        naturalSlideHeight={4}
+                        totalSlides={8}
+                        visibleSlides={4}
+                        isPlaying={true}
+                        infinite={true}
+                        interval={2000}
+                        children={8}
+                        isIntrinsicHeight={true}
+                    >
+
+                        <Slider >
+                            {
+                                languagesSpam.map((element, index) => {
+                                    return (
+                                        <Slide index={index} key={index} >
+                                            <img src={element.src} alt={element.alt} width='115' height='115' gap='2'  />
+                                        </Slide>
+                                    )
+                                })
+                            }
+
+                            {/* <Slide index={0} >
+                        <img src={ReactJsLogo} alt={"ReactJs"}  className='slideImg' style={{height: 'inherit'}}/>
+                    </Slide>
+                    <Slide index={1} >
+                        <img src={ReactJsLogo} alt={"ReactJs"} className='slideImg' />
+                    </Slide>
+                    <Slide index={2} >
+                        <img src={ReactJsLogo} alt={"ReactJs"}  className='slideImg'/>
+                    </Slide>
+                    <Slide index={3} >
+                        <img src={ReactJsLogo} alt={"ReactJs"} className='slideImg' />
+                    </Slide>
+                    <Slide index={4} >
+                        <img src={ReactJsLogo} alt={"ReactJs"} className='slideImg' />
+                    </Slide>
+                    <Slide index={5} >
+                        <img src={ReactJsLogo} alt={"ReactJs"} className='slideImg' />
+                    </Slide> */}
+                        </Slider>
+                    </CarouselProvider>
+                </div>
+            </div>
         </>
     )
 }
